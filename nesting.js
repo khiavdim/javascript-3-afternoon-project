38093237f00b69b912f0e55c0c51141c7100e086
KhiavDim Lee <khiavdim@KhiavDims-MacBook-Pro.local>
2019-06-07 14:33:27 -0600

    working, finished 1-3

--- nesting.js
+++ nesting.js
@@ -115,9 +115,8 @@ var cat = {
 */
 
 //Code Here
-var grumpyActivity;
-var fluffy2ndFriend;
-
+var grumpyActivity = cat.catFriends[0].activities[1];
+var fluffy2ndFriend = cat.catFriends[1].name;
 
 
 ////////// PROBLEM 4 //////////
