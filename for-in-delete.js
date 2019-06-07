a86228a9654876b26c34f4503b0a9002fdaacb4e
KhiavDim Lee <khiavdim@KhiavDims-MacBook-Pro.local>
2019-06-07 16:23:29 -0600

    completed prob 1-2

--- for-in-delete.js
+++ for-in-delete.js
@@ -36,11 +36,17 @@
 ////////// PROBLEM 1 //////////
 
 /*
-  Inside the function showValues, write a for in loop that concatenates each of the property values and returns the concatenated string.
+  Inside the function showValues, write a for in loop that concatenates each of the property values and
+  returns the concatenated string.
 */
 
+//Code here
 function showValues( obj ) {
-  //Code Here
+  let str = ""
+  for(let key in obj){
+    str += obj[key]
+  }
+  return str
 }
 
 
@@ -54,7 +60,14 @@ function showValues( obj ) {
 */
 
 //Code Here
-
+function greaterThan10(obj){
+  for (let key in obj){
+    if (obj[key] > 10){
+      obj[key] = 0
+    }
+  }
+  return obj
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -66,6 +79,8 @@ function showValues( obj ) {
 */
 
 //Code Here
+
+
 
 
 
